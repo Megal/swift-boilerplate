8489995bf96550f8f29176aca7f2d7987e731563
Svyatoshenko "Megal" Misha <m1sha.odesk@gmail.com>
2017-03-14 00:50:11 +0300

    Initial Commit

---
+++ Boilerplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Boilerplate
+//
+//  Created by Svyatoshenko "Megal" Misha on 2017-03-14.
+//  Copyright © 2017 Megal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
